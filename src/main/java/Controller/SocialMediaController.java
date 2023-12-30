5d75b7a3a683718752d192834c420fa473608f90
Robert May <rmmay100@gmail.com>
2023-12-30 22:04:30 +0000

    Commit 1

--- src/main/java/Controller/SocialMediaController.java
+++ src/main/java/Controller/SocialMediaController.java
@@ -1,5 +1,8 @@
 package Controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import io.javalin.Javalin;
 import io.javalin.http.Context;
 
@@ -9,6 +12,14 @@ import io.javalin.http.Context;
  * refer to prior mini-project labs and lecture materials for guidance on how a controller may be built.
  */
 public class SocialMediaController {
+    MessageService messageService;
+    AccountService accountService;
+
+    public SocialMediaController {
+        this.messageService = new MessageService();
+        this.accountService = new AccountService();
+    }
+
     /**
      * In order for the test cases to work, you will need to write the endpoints in the startAPI() method, as the test
      * suite must receive a Javalin object from this method.
@@ -16,7 +27,16 @@ public class SocialMediaController {
      */
     public Javalin startAPI() {
         Javalin app = Javalin.create();
-        app.get("example-endpoint", this::exampleHandler);
+        // app.get("example-endpoint", this::exampleHandler);
+        app.get("/messages", this::getAllMessagesHandler);
+        app.get("/accounts/{account_id}/messages", this::getAllMessagesByUserHandler);
+        app.get("/messages/{message_id}", this::getMessagesByIdHandler);
+        app.post("/messages", this::postMessageHandler);
+        app.patch("/messages/{message_id}", this::updateMessageHandler);
+        app.delete("/messages/{message_id}", this::deleteMessageHandler);
+
+        app.post("/register", this::postAccountHandler);
+        app.post("/login", this::getLoginUser);
 
         return app;
     }
@@ -25,8 +45,22 @@ public class SocialMediaController {
      * This is an example handler for an example endpoint.
      * @param context The Javalin Context object manages information about both the HTTP request and response.
      */
-    private void exampleHandler(Context context) {
-        context.json("sample text");
+    //private void exampleHandler(Context context) {
+    //  context.json("sample text");
+    //}
+
+    private void getAllMessagesHandler(Context ctx) throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
+        List<Message> allMessages = messageService.retrieveAllMessages();
+
+        ctx.json(mapper.writeValueAsString(allMessages));
+    }
+
+    private void getAllMessagesByUserHandler(Context ctx) throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
+        List<Message> userMessages = messageService.retrieveAllMessagesForUser(Integer.parseInt(ctx.pathParam("account_id")));
+
+        ctx.json(mapper.writeValueAsString(userMessages));
     }
 
 
